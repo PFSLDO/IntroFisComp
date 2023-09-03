448bfcca0a3c5f5369d379ee3d831156776e83cd
Pamela Fialho <pamelafialho2002@hotmail.com>
2023-09-03 19:15:03 -0300

    update finalizacao lista 1B

---
+++ lista1B/questao10.c
@@ -0,0 +1,37 @@
+/*
+Indicar qual de três números racionais está mais próximo do número 10.
+*/
+
+#include "stdio.h"
+#include "stdlib.h"
+#include "math.h"
+
+//------------------------------------------------------------------------------------------------------------------------------
+
+int main() {
+  float num1, num2, num3, menorDiferenca;
+
+  printf("Digite um numero racional:\n");
+  scanf("%f", &num1);
+  printf("Digite outro numero racional:\n");
+  scanf("%f", &num2);
+  printf("Digite outro numero racional:\n");
+  scanf("%f", &num3);
+
+  menorDiferenca = abs(10 - num1);
+
+  if (menorDiferenca > (abs(10 - num2))) {
+    menorDiferenca = abs(10 - num2);
+    printf("O numero racional mais proximo do 10 eh: %f\n", num2);
+    return 0;
+  }
+  if (menorDiferenca > (abs(10 - num3))) {
+    menorDiferenca = abs(10 - num3);
+    printf("O numero racional mais proximo do 10 eh: %f\n", num3);
+    return 0;
+  }
+
+  printf("O numero racional mais proximo do 10 eh: %f\n", num1);
+
+  return 0;
+}
--- lista1B/questao9.c
+++ lista1B/questao9.c
@@ -14,13 +14,46 @@ int main() {
   printf("Digite um numero:\n");
   scanf("%f", &num);
 
-  if (num != 0) {
-    printf("Como o numero eh diferente de 0, podemos calcular todas as equacoes:\n");
+  if (num > 0) {
+    printf("O numero eh maior que 0, entao podemos calcular as seguintes:\n");
 
-    //eq1 = ;
+    eq1 = log(num)/num;
+    printf("eq1 = %f\n", eq1);
+
+    if (num < 1.5) {
+      eq2 = 1 / sqrt(-2 * num + 3);
+      printf("eq2 = %f\n", eq2);
+    }
+
+    if (num != 1) {
+      eq3 = log(pow(num, 2) - 1);
+      eq4 = log(1 - pow(num, -2));
+      printf("eq3 = %f\neq4 = %f\n", eq3, eq4);
+    }
+
+    if (num > 3) {
+      eq5 = (sqrt(num - 1)) / (pow(num, 2) - 5 * num + 6);
+      printf("eq5 = % f\n", eq5);
+    }
+  }
+  else if (num == 0) {
+    printf("Como o numero eh 0, podemos calcular apenas as equacoes:\n");
+
+    eq2 = 1 / sqrt(-2 * num + 3);
+    eq4 = log(1 - pow(num,-2));
+    printf("eq2 = %f\neq4 = %f\n", eq2, eq4);
   }
   else {
-    printf("Como o numero eh 0, podemos calcular apenas as equacoes 2, 3, e 4:\n");
+    printf("Como o numero eh negativo, podemos calcular apenas as seguintes equacoes:\n");
+
+    eq2 = 1 / sqrt(-2 * num + 3);
+    printf("eq2 = %f\n", eq2);
+
+    if (num != -1) {
+      eq3 = log(pow(num, 2) - 1);
+      eq4 = log(1 - pow(num, -2));
+      printf("eq3 = %f\neq4 = %f\n", eq3, eq4);
+    }
   }
 
   return 0;
